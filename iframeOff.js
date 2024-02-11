37c83daef91d174b3e7c49712b120a806a7509ad
WebProjectsHub <143710009+WebProjectsHub@users.noreply.github.com>
2024-02-11 20:38:22 +0400

    Update iframeOff.js

--- iframeOff.js
+++ iframeOff.js
@@ -1,8 +1,8 @@
         setTimeout(function() {
             document.getElementById('bodyRight').style.display = 'none';
-        }, 60000);
+        }, 15000);
 //*--------60 seconds----------*//
         setTimeout(function() {
             document.getElementById('bodyRight').style.display = 'block';
-        }, 30000);
-//*--------60+60 seconds--------*//
+        }, 35000);
+
